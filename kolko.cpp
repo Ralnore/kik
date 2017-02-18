8619c9db60de52cc7da954270c257e3672b334dc
Ralnore <gorn244@gmail.com>
2017-02-18 14:53:27 +0100

    Jest ruch gracza. Nie wystwietla liter

--- kolko.cpp
+++ kolko.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
+char jakiZnak;
+int jakiePole;
 int plansza[2][2];
+char znaki[2]={'X','O'};
+void wyborZnaku(){
+    cout<<"Jezeli chcesz grać krzyżykiem wciśnij X, jeżeli kółkiem wciśnij O";
+      cin>>jakiZnak;
+      if(jakiZnak=='X')
+        cout<<"Grasz Iksem"<<endl;
+      else if(jakiZnak=='O')
+        cout<<"Grasz kolkiem"<<endl;
+      else
+        cout<<"Nie ma takiego znaku. Wypierdalaj";
+}
+
 void wypelnijTablice(){
   int temp=1;
   for(int i=0;i<3;i++)
@@ -22,10 +36,23 @@ void rysujPlansze()
   }
 }
   void ruchGracza(){
-    
+    cout<<"Podaj numer pola"<<endl;
+    cin>>jakiePole;
+    if(jakiePole<=3)
+      plansza[0][jakiePole-1]=jakiZnak;
+    else if(jakiePole<=6 && jakiePole>=4)
+      plansza[1][jakiePole-4]=jakiZnak;
+    else if(jakiePole<=9 && jakiePole>=7)
+      plansza[2][jakiePole-7]=jakiZnak;
+
+
   }
 int main(){
+  wyborZnaku();
   wypelnijTablice();
+  while(1){
   rysujPlansze();
+  ruchGracza();
+}
   return 0;
 }
